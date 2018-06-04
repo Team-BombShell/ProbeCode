9efbd0f1f0c5e9e430ac35a79b3fb48d359c01a9
AmJa202 <amber.james202@gmail.com>
2018-06-04 14:05:57 -0500

    RTC lul

---
+++ Prototype/OneMonth/src/DRIVERS/RTC.c
@@ -0,0 +1,6 @@
+/*
+ * RTC.c
+ *
+ * Created: 6/4/2018 1:38:45 PM
+ *  Author: amber
+ */ 
---
+++ Prototype/OneMonth/src/DRIVERS/RTC.h
@@ -0,0 +1,16 @@
+/*
+ * RTC.h
+ *
+ * Created: 6/4/2018 1:38:57 PM
+ *  Author: amber
+ */ 
+
+
+#ifndef RTC_H_
+#define RTC_H_
+
+
+
+
+
+#endif /* RTC_H_ */
